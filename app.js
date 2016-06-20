468af37a74d34f97a075d5af42758883c271d380
tezml <tezml@tezml.com>
2016-06-20 09:28:38 +0800

    false  data

--- app.js
+++ app.js
@@ -20,12 +20,26 @@ app.get('/',function(req,res){
 });
 app.get('/blog/',function(req,res){
     res.render("blog",{
-        classSeleced:"blog"
+        classSeleced:"blog",
+        blog:[
+            {id:"1",title:"我是博客名字1",outline:"我是简介1",createTime:"2015/08/20",titleImg:"/img/work1.jpg"},
+            {id:"2",title:"我是博客名字2",outline:"我是简介2",createTime:"2015/08/21",titleImg:"/img/work1.jpg"},
+            {id:"3",title:"我是博客名字3",outline:"我是简介3",createTime:"2015/08/22",titleImg:"/img/work1.jpg"},
+            {id:"4",title:"我是博客名字4",outline:"我是简介4",createTime:"2015/08/23",titleImg:"/img/work1.jpg"},
+            {id:"5",title:"我是博客名字5",outline:"我是简介5",createTime:"2015/08/24",titleImg:"/img/work1.jpg"}
+        ]
     })
 });
 app.get('/gallery/',function(req,res){
     res.render("gallery",{
-        classSeleced:"gallery"
+        classSeleced:"gallery",
+        gallery:[
+            {id:"1",titleImg:"/img/work1.jpg",title:"代码"},
+            {id:"2",titleImg:"/img/work2.jpg",title:"足球"},
+            {id:"3",titleImg:"/img/work3.jpg",title:"热爱"},
+            {id:"4",titleImg:"/img/work4.jpg",title:"梦想"},
+            {id:"5",titleImg:"/img/work5.jpg",title:"其他"}
+        ]
     })
 });
 app.get('/board/',function(req,res){
@@ -38,14 +52,16 @@ app.get('/contact/',function(req,res){
         classSeleced:"contact"
     })
 });
-app.get('/inner/',function(req,res){
+app.get('/inner/:id',function(req,res){
     res.render("inner",{
-        classSeleced:"Tezml"
+        classSeleced:"Tezml",
+        data:{title:"我是博客名字1",id:"1"}
     })
 });
-app.get('/photo/',function(req,res){
+app.get('/photo/:id',function(req,res){
     res.render("photo",{
-        classSeleced:"Tezml"
+        classSeleced:"Tezml",
+        data:{id:"1"}
     })
 });
 
--- public/js/tezml.js
+++ public/js/tezml.js
@@ -2,6 +2,5 @@
 $(function() {
     //判断当前页面选中哪个html
     var cClass="."+$(".bar ul").attr("bar");
-    console.log(cClass);
     $(".bar ul").find(cClass).addClass("selected")
 });
