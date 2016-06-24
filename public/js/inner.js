271a6214e8357cac6b07fb0613102ffbcf8481fa
tezml <tezml@tezml.com>
2016-06-24 11:41:09 +0800

    依赖加载

---
+++ public/js/inner.js
@@ -0,0 +1,14 @@
+$(function(){
+    var _id=$(".content").attr("sid");
+    $.ajax({
+        url: '/blog/content/',
+        data: {
+            _id:_id
+        },
+        type: 'post',
+        dataType: 'json',
+        success:function(res){
+            $(".content").append(res.inner);
+        }
+    })
+});
